bc7492577286f681383c3bc75f2e83752f4cca47
Anna-Ashton <ann.karanja@student.moringaschool.com>
2022-10-26 23:06:40 +0300

    Completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+
+    attr_accessor :new_word
+
+    def initialize(new_word)
+        @new_word = new_word
+    end
+
+    def match(arr_of_words)
+        matches = []
+        arr_of_words.each do |word|
+            if word.chars.sort == @new_word.chars.sort
+                matches.push(word)
+            end
+        end
+        matches
+    end
+
+end
